f36942045ff10a5a03473fa06922099feab9fb52
Amine Bellakrid <a.bellakrid@gmail.com>
2011-06-25 14:31:24 +0200

    premier commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all(:order => "created_at DESC")
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+    @post = Post.create(:message => params[:message] , :user_id => current_user.id)
+    
+   
+    respond_to do |format|
+      if @post.save
+
+        format.html { redirect_to posts_path }
+      else
+        flash[:notice] = "Message failed to save."
+        format.html { redirect_to posts_path }
+      end
+    end
+  end
+  
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meantthat::Application.config.secret_token = '4d345391fbe6c25db0d4347536c77064819e583a101fed95f6a24f6cbad172b455c771e653eb1653ace5066951cf1be3719a0e8bcbdccbfba1017aa7bd2104bd'
